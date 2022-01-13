53552d698f55fdca77e0406ef662e0f95003da5c
Germano Alves <germanomacedoalves@hotmail.com>
2022-01-13 14:13:27 -0300

    version 1.2

--- src/components/template/Cardstree.tsx
+++ src/components/template/Cardstree.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 export default function Cardtree() {
   return (
    <div>
-      <div className="max-w-6xl mx-auto px-4 lg:px-9 mt-2 md:mt-6 mb-12">
+      <div className="max-w-6xl mx-auto px-4 lg:px-9 -mt-14 md:mt-6 mb-12">
          <h1 className="text-blue font-mt font-bold text-tth1 mb-2">Saiba quem nós somos e o que podemos fazer por você, e pelo seu negócio.</h1>
          <p className="text-titles text-2xftsqr font-pp">Nós somos o Maestrus, uma empresa especialista em criação de plataformas digitais para o ensino a distância.<br/><br/>
             Escolas, professores e empresas que queiram oferecer algum curso ou treinamento de maneira online, totalmente a distância, podem hospedar o seu conteúdo sem dificuldades ou dores de cabeça em nossa plataforma, enquanto nós:
