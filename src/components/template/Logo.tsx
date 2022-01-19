30fefd9d0c64dda5324bd59fe15bfc92b90e77d7
Germano Alves <germanomacedoalves@hotmail.com>
2022-01-19 17:40:29 -0300

    version 1.6

--- src/components/template/Logo.tsx
+++ src/components/template/Logo.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import Link from 'next/link'
 export default function Logo() {
    return (
-   <div className=" z-10 flex justify-center items-center -mb-28 -ml-4 sm:absolute sm:ml-52 md:mb-0 md:ml-20 ">
+   <div className=" z-10 flex justify-center items-center -mb-28 -ml-4 sm:absolute sm:ml-44 md:mb-0 md:ml-5 lg:ml-5">
      <a href="https://maestrus.com"><Image src="/images/logomaestrusii.png" width={300} height={77} alt="Logotipo Maestrus"/></a>
    </div>
    )
