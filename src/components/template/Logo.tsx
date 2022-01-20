fe1ef78c18ba216114e5b2f5ae5af747dd18c21c
Germano Alves <germanomacedoalves@hotmail.com>
2022-01-20 10:46:11 -0300

    version 1.1

--- src/components/template/Logo.tsx
+++ src/components/template/Logo.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import Link from 'next/link'
 export default function Logo() {
    return (
-   <div className=" z-10 flex justify-center items-center -mb-28 -ml-4 sm:absolute sm:ml-44 md:mb-0 md:ml-5 lg:ml-5">
+   <div className="z-10 flex justify-center items-center -mb-28 -ml-4 sm:absolute sm:ml-44 md:mb-0 md:ml-4 lg:ml-14">
      <a href="https://maestrus.com"><Image src="/images/logomaestrusii.png" width={300} height={77} alt="Logotipo Maestrus"/></a>
    </div>
    )
--- src/components/template/Top.tsx
+++ src/components/template/Top.tsx
@@ -3,13 +3,13 @@ import Image from 'next/image';
 
 export default function Top() {
     return (
-         <div className="flex justify-center items-center max-w-7xl mx-auto px-4 sm:mt-28 md:mt-20 lg:-mt-38 lg:-mb-12 xl:-mt-20">
-            <div className="grid justify-center place-items-center sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-2 gap-2">
-                <div>
-                     <h1 className="text-blue font-mt font-bold text-tth1 mb-4 text-left">Preserve a qualidade, os valores<br/>e a cultura da sua empresa, mesmo em home office.</h1>
+         <div className="flex max-w-7xl mx-auto mt-32 sm:mt-28 md:mt-20 lg:-mt-38 lg:-mb-12 xl:-mt-20">
+            <div className="grid gap-1 justify-center place-items-center sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-2">
+                <div className="px-4 lg:ml-14">
+                     <h1 className="text-blue font-mt font-bold text-tth1 mb-4 text-left">Preserve a qualidade, os valores e a cultura da sua empresa, mesmo em home office.</h1>
                      <p className="text-titles text-2xftsqr font-pp text-left">Diminua gastos com aluguéis, infraestrutura e deslocamente. Faça entrevistas, contrate e <strong>treine</strong> toda a sua equipe,<br/>através da internet.</p>
                 </div>
-                <div>
+                <div className="m-0 pr-1 lg:-mt-10 xl:mt-0 ">
                     <Image src="/images/topo_modeloii.png" width={366}  height={349}></Image>                  
                 </div>
             </div>
