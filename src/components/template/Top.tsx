39c8a3ba2f2bbd20aca7b1f4ec47b1379d031175
Germano Alves <germanomacedoalves@hotmail.com>
2022-02-02 16:46:49 -0300

    version 5.2 router

--- src/components/template/Top.tsx
+++ src/components/template/Top.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable jsx-a11y/alt-text */
 import Image from 'next/image';
+import Link from 'next/link'
 
 export default function Top() {
     return (
@@ -7,7 +8,9 @@ export default function Top() {
                 <div  className="top-0">
                     <div className="max-w-7xl mx-auto flex flex-wrap items-center mt-0 py-2">
                         <div className="pl-4 flex items-center">
-                            <a href="/"><Image src="/images/logomaestrusii.png" width={300} height={77} alt="Logotipo Maestrus"/></a>
+                            <Link href="/" passHref={true}>
+                                <a><Image src="/images/logomaestrusii.png" width={300} height={77} alt="Logotipo Maestrus"/></a>
+                            </Link>
                         </div>
                     </div>
                 </div>
